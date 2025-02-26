f386ed3ddc3e281094f83d85d0e2564fd54ee6bc
Lyttr <jgy2001@126.com>
2025-02-25 19:46:48 -0800

    Initial commit

---
+++ src/test/java/com/logicalshovel/mytwitch/MytwitchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logicalshovel.mytwitch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytwitchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
